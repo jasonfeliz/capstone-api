f4e77a97df8c2742ae40f3d6e459c25e2f9c5d5e
jasonrfeliz <jason@meetmycampus.com>
2019-01-15 20:44:26 -0500

    config routes

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class ApplicationRecord
-  include Mongoid::Document
-  self.abstract_class = true
-end
+# class ApplicationRecord
+#   include Mongoid::Document
+#   self.abstract_class = true
+# end
